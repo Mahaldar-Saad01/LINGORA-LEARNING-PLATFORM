d69190eff21223b91faed0f3a4eeccb21ffb2dfe
Mahaldar Saad <onyx.saad@gmail.com>
2026-08-19 23:06:08 +0530

    settings cors

--- Backend_app/app/config/settings.py
+++ Backend_app/app/config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['127.0.0.1',"lingora-learning-platform.onrender.com"]
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -49,6 +50,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -56,6 +58,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000","https://lingora-learning-platform-1.onrender.com"
 ]
 
 ROOT_URLCONF = 'config.urls'
